09d2ed61b9eb70a749a0b798f3bc9330881b3dc0
AliScripter <araz.hello@gmail.com>
2025-03-28 14:01:51 +0330

    feat: add dropdown + category page

--- include/layouts/header.php
+++ include/layouts/header.php
@@ -19,6 +19,10 @@ include_once $base_path . 'include/config.php';
 include_once $base_path . 'include/db.php';
 include_once $base_path . 'include/helpers.php';
 
+
+
+$categories = $db->query("SELECT * FROM categories")->fetchAll();
+
 ?>
 
 <!DOCTYPE html>
@@ -45,7 +49,7 @@ include_once $base_path . 'include/helpers.php';
     <!-- Menu -->
     <nav class="bg-white border-gray-200 dark:bg-gray-900">
         <div
-            class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 items-center">
             <a
                 href="../../../php_shop/index.php"
                 class="flex items-center space-x-3 rtl:space-x-reverse">
@@ -54,7 +58,7 @@ include_once $base_path . 'include/helpers.php';
                     class="h-8"
                     alt="Flowbite Logo" />
                 <span
-                    class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                    class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Tech Store</span>
             </a>
             <div class="flex md:order-2">
                 <button
@@ -159,7 +163,7 @@ include_once $base_path . 'include/helpers.php';
                     </form>
                 </div>
                 <ul
-                    class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 transition-all delay-150 duration-300 ease-in-out">
+                    class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 transition-all delay-150 duration-300 ease-in-out items-center">
                     <li>
                         <a
                             href="#"
@@ -172,9 +176,23 @@ include_once $base_path . 'include/helpers.php';
                             class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
                     </li>
                     <li>
-                        <a
-                            href="#"
-                            class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+                        <!------------ Dropdown -->
+                        <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="text-white  font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center cursor-pointer" type="button">Menu <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
+                            </svg>
+                        </button>
+
+                        <!-- Dropdown menu -->
+                        <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700">
+                            <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+                                <?php foreach ($categories as $category) : ?>
+                                    <li>
+                                        <a href="<?php echo getPageUrl('products.php?id=' . $category['id']); ?>" class="capitalize block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                                            <?= $category['name'] ?></a>
+                                    </li>
+                                <?php endforeach ?>
+                            </ul>
+                        </div>
                     </li>
                 </ul>
             </div>
--- pages/products.php
+++ pages/products.php
@@ -1,13 +1,31 @@
 <?php
 include_once "../include/layouts/header.php";
 
-// Get all products from database
-$stmt = $db->query("SELECT * FROM products");
-$products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// Start of Selection
+if (isset($_GET['id'])) {
+    $categoryId = (int) $_GET['id'];
+    $stmt = $db->prepare("SELECT * FROM products WHERE category_id = :id ;");
+    $stmt->execute(['id' => $categoryId]);
+    $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    //! Displaying products from a specific category
+    $categoryStmt = $db->prepare("SELECT name FROM categories WHERE id = :id");
+    $categoryStmt->execute(['id' => $categoryId]);
+    $category = $categoryStmt->fetch(PDO::FETCH_ASSOC);
+} else {
+    // Get all products from database
+    $stmt = $db->query("SELECT * FROM products");
+    $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
 ?>
 
 <div class="container mx-auto px-4 py-8">
-    <h1 class="text-2xl font-bold mb-4">All Products</h1>
+    <?php if (isset($_GET['id'])) : ?>
+        <h1 class="text-2xl font-bold capitalize mb-4"><?= htmlspecialchars($category['name']) ?> Products</h1>
+    <?php else : ?>
+        <h1 class="text-2xl font-bold capitalize mb-4">All Products</h1>
+    <?php endif ?>
 
     <?php if (count($products) > 0): ?>
         <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
